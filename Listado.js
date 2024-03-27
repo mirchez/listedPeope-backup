e2ad99152f11470232344ce4637ccff6fabd4117
Miguel <mmirandasanchez16@gmail.com>
2024-03-27 12:35:27 -0400

    copia de seguridad

--- Listado.js
+++ Listado.js
@@ -10,3 +10,17 @@ function mostrarPersonas() {
   }
   document.getElementById("personas").innerHTML = texto;
 }
+
+function agregarPersona() {
+  const forma = document.forms["forma"];
+  const nombre = forma["nombre"];
+  const apellido = forma["apellido"];
+  if ((nombre.value && apellido.value) != "") {
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+  } else {
+    console.log("No hay datos que agregar");
+  }
+}
